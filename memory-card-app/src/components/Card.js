34b1a2c52da3b9614fa6fad7d642e0cc278e41c5
James Graca-Jones <jamesgracajones@gmail.com>
2023-02-06 16:56:10 +0000

    Write an image order randomiser and pass to img onClick

--- memory-card-app/src/components/Card.js
+++ memory-card-app/src/components/Card.js
@@ -6,7 +6,11 @@ function Card(props) {
             {/* TODO: The src is passed to alt as well so people who cannot see 
             can try to remember the links they have chosen; however, should 
             replace each alt with a description specific to the image.*/}
-            <img src={props.imgSrc} alt={props.imgSrc}></img>
+            <img
+                src={props.imgSrc}
+                alt={props.imgSrc}
+                onClick={props.onImgClick}
+            />
         </div>
     )
 }
--- memory-card-app/src/components/Deck.js
+++ memory-card-app/src/components/Deck.js
@@ -5,10 +5,16 @@ import "./Deck.css";
 function Deck(props) {
     const [imagesInOrder, setImagesInOrder] = useState(props.imagesInOrder);
 
+    const randomiseImagesOrder = () => {
+        const newImagesInOrder = JSON.parse(JSON.stringify(imagesInOrder));
+        newImagesInOrder.sort((a, b) => 0.5 - Math.random());
+        setImagesInOrder(newImagesInOrder);
+    };
     const makeCard = (key, imgSrc) => {
         return <Card
             key={key}
             imgSrc={imgSrc}
+            onImgClick={randomiseImagesOrder}
         />
     };
     const makeCardList = () => {
